74534f790a04065a5fef9f3d2e80a0c2a258da95
WickyNilliams <WickyNilliams@MBA>
2017-09-20 23:14:44 +0100

    improve example
    * es6
    * overall better example of usefulness

--- example/App.js
+++ example/App.js
@@ -1,29 +1,60 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Clipboard = require("react-clipboard");
+import React from "react";
+import ReactDOM from "react-dom";
+import Clipboard from "react-clipboard";
 
-var App = React.createClass({
+class Mouse extends React.Component {
+  handleMove = ({ clientX, clientY }) => {
+    this.props.onMove({ x: clientX, y: clientY });
+  };
 
-  render: function() {
-    var value = JSON.stringify(this.props.data, null, "  ");
+  componentDidMount() {
+    document.addEventListener("mousemove", this.handleMove);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousemove", this.handleMove);
+  }
+
+  render() {
+    return nutll;
+  }
+}
+
+class App extends React.Component {
+  state = {
+    pos: {
+      x: 0,
+      y: 0
+    },
+    copied: false
+  };
+
+  handleCopy = () => {
+    clearTimeout(this.timer);
+    this.setState({ copied: true });
+    this.timer = setTimeout(() => this.setState({ copied: false }), 1000);
+  };
+
+  handleMove = pos => {
+    this.setState({ pos });
+  };
+
+  render() {
+    const { pos, copied } = this.state;
+    const value = JSON.stringify(pos, null, "  ");
 
     return (
       <div>
-        <p>Press Cmd + C to copy:</p>
-        <pre>{value}</pre>
+        <Mouse onMove={this.handleMove} />
+        <p>Press Cmd + C to copy mouse position</p>
+        <pre>
+          ({pos.x},{pos.y})
+        </pre>
         <Clipboard value={value} onCopy={this.handleCopy} />
+        {copied && <p>copied</p>}
       </div>
     );
-  },
-
-  handleCopy : function(e) {
-    console.log("copied", e);
   }
+}
 
-});
-
-var data = { foo : 1, bar : 2 };
-ReactDOM.render(
-  <App data={data} />,
-  document.querySelector("#app")
-);
+ReactDOM.render(<App />, document.querySelector("#app"));
